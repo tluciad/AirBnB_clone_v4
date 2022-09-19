72abbaf9be592697de3d811951e2cb802c8abbd7
Edwin-0396 <egc4611@gmail.com>
2022-09-19 11:20:58 -0900

    new updates

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -20,7 +20,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/1-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -37,7 +37,7 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
     
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places, cache_id = uuid.uuid4())
@@ -45,4 +45,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
