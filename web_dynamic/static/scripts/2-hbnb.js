103acd9010133fb766a40ecce97dcfb47ec1cb0c
Edwin-0396 <egc4611@gmail.com>
2022-09-19 17:51:34 -0900

    new updates

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+	let checkamenity = {};
+	$(document).on('change', "input[type='checkbox']", function () {
+		if (this.checked){
+			checkamenity[$(this).data('id')] = $(this).data('name');
+		} else {
+			delete checkamenity[$(this).data('id')];
+		}
+		let list = Object.values(checkamenity);
+		if (list.length > 0){
+			$('div.amenities > h4').text(Object.values(checkamenity).join(', '));
+		} else {
+			$('div.amenities > h4').html('&nbsp;');
+		}
+	});
+	$get('http://0.0.0.0:5001/api/v1/status/', function(data, Status){
+		if (Status === 'success') {
+			if (data.status === 'OK') {
+				$('#api_status').addClass('available')
+			} else {
+				$('#api_status').removeClass('available')
+			}
+		}
+	});
+});
